5eacdfb795f2408b7657289d5c37a4dd4ef49ef3
PECQUE <aubeline.pecque@hotmail.com>
2021-05-19 16:02:13 +0200

    Mapping Particulier, Entreprise, Association

--- src/main/java/sopra/ShareYourFood/repository/IMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package sopra.ShareYourFood.repository;
-
-import sopra.ShareYourFood.model.Message;
-
-public class IRepositoryMessage extends IRepository<Message, Long>{
-
-}
--- src/main/java/sopra/ShareYourFood/repository/IProduitRepository.java
+++
@@ -1,7 +0,0 @@
-package sopra.ShareYourFood.repository;
-
-import sopra.ShareYourFood.model.Produit;
-
-public class IRepositoryProduit extends IRepository<Produit, Long>{
-
-}
