4758d4a62bf946b320f0cf46a31311974f107208
Geoffrey <fb>
2021-05-19 15:00:56 +0200

    Lot,ProduitLot,ProduitLotId

--- src/main/java/sopra/ShareYourFood/model/Lot.java
+++ src/main/java/sopra/ShareYourFood/model/Lot.java
@@ -18,5 +18,115 @@ public class Lot {
 	private Date dtPeremptionLot;
 	private String photo;
 	private Statut statut;
-	
+	private Don don;
+
+	public Lot() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Long getVolume() {
+		return volume;
+	}
+
+	public void setVolume(Long volume) {
+		this.volume = volume;
+	}
+
+	public Date getDtPeremptionLot() {
+		return dtPeremptionLot;
+	}
+
+	public void setDtPeremptionLot(Date dtPeremptionLot) {
+		this.dtPeremptionLot = dtPeremptionLot;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public Statut getStatut() {
+		return statut;
+	}
+
+	public void setStatut(Statut statut) {
+		this.statut = statut;
+	}
+
+	public Don getDon() {
+		return don;
+	}
+
+	public void setDon(Don don) {
+		this.don = don;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dtPeremptionLot == null) ? 0 : dtPeremptionLot.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + ((photo == null) ? 0 : photo.hashCode());
+		result = prime * result + ((volume == null) ? 0 : volume.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lot other = (Lot) obj;
+		if (dtPeremptionLot == null) {
+			if (other.dtPeremptionLot != null)
+				return false;
+		} else if (!dtPeremptionLot.equals(other.dtPeremptionLot))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (photo == null) {
+			if (other.photo != null)
+				return false;
+		} else if (!photo.equals(other.photo))
+			return false;
+		if (volume == null) {
+			if (other.volume != null)
+				return false;
+		} else if (!volume.equals(other.volume))
+			return false;
+		return true;
+	}
+
 }
